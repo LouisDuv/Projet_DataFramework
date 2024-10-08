64fcee768c3287433d6d6b4251648ec26bbb531e
Mael Depréville <mael.depreville@edu.ece.fr>
2024-10-08 21:53:50 +0200

    functions add

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import yfinance as yf
 
+import numpy as np
+
 import glob
 import os
 
 import findspark
 findspark.init("C:\Program Files\spark-3.5.3-bin-hadoop3")
 
-import pyspark as ps
-from pyspark.sql import SparkSession
+from pyspark.sql import functions as sf
+from pyspark.sql.types import NumericType
 
 from DataFrame import DataframeClass
 
@@ -28,6 +30,32 @@ def head_and_tail_40(df):
     tail_40 = df.orderBy("Date", ascending=False).limit(40).orderBy("Date", ascending=True)
     return head_40.union(tail_40)
 
+def num_observations(df):
+    return df.count()
+
+def descript_stats(df):
+    agg_expr = []
+    for column in df.columns:
+        agg_expr.append(sf.min(column).alias(f"min_{column}"))
+        agg_expr.append(sf.max(column).alias(f"max_{column}"))
+        if isinstance(df.schema[column].dataType, NumericType):
+            agg_expr.append(sf.stddev(column).alias(f"stddev_{column}"))
+    result = df.agg(*agg_expr)
+    return result
+
+def count_missing(df):
+    missing_expr = []
+    for column in df.columns:
+        if isinstance(df.schema[column].dataType, NumericType):
+            missing_expr.append(
+                sf.count(sf.when(sf.col(column).isNull() | sf.isnan(column), column)).alias(f"missing_{column}")
+            )
+        else:
+            missing_expr.append(
+                sf.count(sf.when(sf.col(column).isNull(), column)).alias(f"missing_{column}")
+            )
+    return df.select(missing_expr)
+
 dataframe_obj = DataframeClass()
 
 csv_folder_path = 'Stocks_Price'
@@ -35,9 +63,8 @@ csv_files = glob.glob(os.path.join(csv_folder_path, "*.csv"))
 
 data_df = dataframe_obj.read_multiple_csv(csv_files)
 
-# dataframe_obj.print_schemas()
-
-result = dataframe_obj.perform_operation_on_each(head_and_tail_40)
+result = dataframe_obj.perform_operation_on_each(count_missing)
 for idx, res in enumerate(result):
     print(f"Result for dataframe {idx+1}:")
-    res.show(80)
+    res.show()
+    
